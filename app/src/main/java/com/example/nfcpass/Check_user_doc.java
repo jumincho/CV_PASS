13e6622a2636e85077c0176b21cb24c05d02b951
after <aftermoa@gmail.com>
2021-11-20 19:32:06 +0900

    update lay

---
+++ app/src/main/java/com/example/nfcpass/Check_user_doc.java
@@ -0,0 +1,14 @@
+package com.example.nfcpass;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Check_user_doc extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_check_user_doc);
+    }
+}
