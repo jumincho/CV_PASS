be3ad2217d9dee151bc5a1676dee9d01d0132bf3
after <aftermoa@gmail.com>
2021-11-26 03:41:58 +0900

    cv-pass ver2

--- app/src/main/java/com/example/nfcpass/history.java
+++ app/src/main/java/com/example/nfcpass/history.java
@@ -6,11 +6,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
